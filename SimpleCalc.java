1bc434f17f7bf2961904bbe708f2727f7f798598
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2018-11-22 10:11:14 -0600

    Initial upload
    Calculator class
    and User class called APP

---
+++ SimpleCalc.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.simplecalculatortwo;
+
+/**
+ *
+ * @author bacag
+ */
+class SimpleCalc {
+    
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public static int divide(int a, int b) {
+        return a/b;
+    }
+}
